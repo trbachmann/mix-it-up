d62a5ad55657b20f0e527389ebb4d1871f8e4fdd
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-21 14:51:21 -0700

    Add fetchData

---
+++ src/utils/fetchData.js
@@ -0,0 +1,8 @@
+
+export const fetchData = async (url) => {
+  const response = await fetch(url);
+  if(!response.ok) {
+    throw new Error(response.message)
+  } 
+   return await response.json();
+}
