68eb8fd3a7042a29db2bf65a4b600f8bb76cb3ee
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-21 16:30:42 -0700

    Add routes for desserts my-recipes and desserts/:id

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Switch, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { fetchMatches } from '../../thunks/fetchMatches';
+import RecipeContainer from '../RecipeContainer/RecipeContainer.js';
+import RecipeCard from '../RecipeCard/RecipeCard.js';
+import { Error404 } from '../../components/Error404/Error404.js';
 
 export class App extends Component {
 
   componentDidMount() {
     this.props.fetchMatches();
   }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header className="App--header">
           <h1>Mix It Up</h1>
         </header>
+        <Switch>
+          <Route exact path='/' render={({ match }) => <RecipeContainer match={match}/>}/>
+          <Route path='/desserts' render={({ match }) => <RecipeContainer match={match}/>} />
+          <Route path='/my-recipes' render={({ match }) => <RecipeContainer match={match}/>} />
+        </Switch>
+        <Route path='/desserts/:id' render={({ match }) => {
+          const dessert= this.props.desserts.find(dessert => dessert.id === match.params.id);
+          if (!dessert) {
+            return <Error404 />
+          }
+          return <RecipeCard match={match} {...dessert}/>
+        }} />
       </div>
     );
   }
