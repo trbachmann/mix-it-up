45c4d1ae2309c0f1200d228f839ae5f15378fbe8
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-21 16:31:51 -0700

    Add RecipeContainer with getRecipeType and getRecipesToDisplay

---
+++ src/containers/RecipeContainer/RecipeContainer.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { RecipeLinkCard } from '../../components/RecipeLinkCard/RecipeLinkCard';
+
+export class RecipeContainer extends Component {
+  getRecipeType = () => {
+    const { match } = this.props;
+    switch(match.path) {
+      case '/my-recipes':
+        return 'userRecipes';
+      case '/desserts':
+        return 'desserts';
+      case '/':
+        return 'desserts';
+      default:
+        return 'desserts';
+    }
+  }
+  
+  getRecipesToDisplay = () => {
+    let property = this.getRecipeType();  
+    return this.props[property].map(recipe => {
+      return <RecipeLinkCard recipe={recipe} key={recipe.id}/>
+    });
+  }
+
+  render() {
+    return(
+      <div className='RecipeContainer'>
+      {this.getRecipesToDisplay()}
+      </div>
+    )
+  }
+}
+
+export const mapStateToProps = (state) => ({
+  desserts: state.desserts,
+  userRecipes: state.userRecipes
+});
+
+export default connect(mapStateToProps)(RecipeContainer);
