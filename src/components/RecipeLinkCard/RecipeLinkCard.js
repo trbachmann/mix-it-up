624a7313cd2d7716d724fd2d9c3f3157b8297a31
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-22 12:39:05 -0700

    Style RecipeLinkCard

--- src/components/RecipeLinkCard/RecipeLinkCard.js
+++ src/components/RecipeLinkCard/RecipeLinkCard.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 export const RecipeLinkCard = (props) => {
   const { recipe } = props;
   return(
-    <Link to={'/desserts/'+ recipe.id}>
-      <div className='RecipeLinkCard'>
-        <img src={recipe.images[0].hostedLargeUrl}/>
+    <Link className='RecipeLinkCard--Link' to={'/desserts/'+ recipe.id}>
+      <div className='RecipeLinkCard--div'>
+        <img className='RecipeLinkCard-img' src={recipe.images[0].hostedLargeUrl} alt={recipe.name}/>
         <h2>{recipe.name}</h2>
         <p>Time: {recipe.totalTime}</p>
         <p>From: {recipe.source.sourceDisplayName}</p>
