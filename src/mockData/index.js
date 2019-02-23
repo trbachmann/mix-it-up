341ae8de4f8ac577ba7172db7f83455de513f973
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-22 23:17:49 -0700

    Add mock data for testing

---
+++ src/mockData/index.js
@@ -0,0 +1,167 @@
+export const mockRecipes = [
+  {
+    yield: "4 Servings",
+    totalTime: "15 min",
+    images: [
+      {
+        hostedSmallUrl: "http://lh4.ggpht.com/eUAHA16-zMjrkoD1qSyD-5GVyckd__2WwY213f-rIi7NfZbCmwfQ53pN9COxZ6q9VZBTbc0weJvJhy8NoVymLQ=s90",
+        hostedMediumUrl: "http://lh4.ggpht.com/eUAHA16-zMjrkoD1qSyD-5GVyckd__2WwY213f-rIi7NfZbCmwfQ53pN9COxZ6q9VZBTbc0weJvJhy8NoVymLQ=s180",
+        hostedLargeUrl: "http://lh4.ggpht.com/eUAHA16-zMjrkoD1qSyD-5GVyckd__2WwY213f-rIi7NfZbCmwfQ53pN9COxZ6q9VZBTbc0weJvJhy8NoVymLQ=s360",
+        imageUrlsBySize: {
+          90: "http://lh5.ggpht.com/BtZ6SmVreIVczKceXBa6Chq79PajwSVK047clqtMnSCQ5nlnnfhCEhSl6xzwDoovGG1eVzUnH1NUoHgXazCH=s90-c",
+          360: "http://lh5.ggpht.com/BtZ6SmVreIVczKceXBa6Chq79PajwSVK047clqtMnSCQ5nlnnfhCEhSl6xzwDoovGG1eVzUnH1NUoHgXazCH=s360-c"
+        }
+      }
+    ],
+    name: "Toasted Marshmallow Cookies and Cream Milkshake",
+    source: {
+      sourceDisplayName: "Picky Palate",
+      sourceSiteUrl: "http://picky-palate.com",
+      sourceRecipeUrl: "http://picky-palate.com/2013/02/15/toasted-marshmallow-cookies-and-cream-milkshake/"
+    },
+    id: "Toasted-marshmallow-cookies-and-cream-milkshake-335047",
+    ingredientLines: [
+      "6 Jumbo marshmallows",
+      "6 cups good quality vanilla ice cream",
+      "1 cup milk",
+      "6 Oreo Cookies"
+    ],
+    attribution: {
+      html: "<a href='http://www.yummly.co/recipe/Toasted-marshmallow-cookies-and-cream-milkshake-335047'>Toasted Marshmallow Cookies and Cream Milkshake recipe</a> information powered by <img alt='Yummly' src='https://static.yummly.co/api-logo.png'/>",
+      url: "http://www.yummly.co/recipe/Toasted-marshmallow-cookies-and-cream-milkshake-335047",
+      text: "Toasted Marshmallow Cookies and Cream Milkshake recipes: information powered by Yummly",
+      logo: "https://static.yummly.co/api-logo.png"
+    },
+    numberOfServings: 4,
+    totalTimeInSeconds: 900,
+    attributes: {
+      course: [
+        "Beverages",
+        "Desserts"
+      ],
+      cuisine: [
+        "Kid-Friendly"
+      ]
+    },
+    "flavors": {
+      Piquant: 0,
+      Meaty: 0.1667,
+      Bitter: 0.1667,
+      Sweet: 0.6667,
+      Sour: 0.1667,
+      Salty: 0.1667
+    },
+    rating: 5
+  },
+  {
+    yield: "serves two",
+    totalTime: "40 min",
+    images: [
+      {
+        hostedSmallUrl: "https://lh3.googleusercontent.com/IL4SAQ143siXT-QfTZjeNORYdW5BTxDXLdQw-Ix1-rBMLlTfHKhZZFajd3myiwdIxndI9Kv3mpdzetFJPz7qfQ=s90",
+        hostedMediumUrl: "https://lh3.googleusercontent.com/IL4SAQ143siXT-QfTZjeNORYdW5BTxDXLdQw-Ix1-rBMLlTfHKhZZFajd3myiwdIxndI9Kv3mpdzetFJPz7qfQ=s180",
+        hostedLargeUrl: "https://lh3.googleusercontent.com/IL4SAQ143siXT-QfTZjeNORYdW5BTxDXLdQw-Ix1-rBMLlTfHKhZZFajd3myiwdIxndI9Kv3mpdzetFJPz7qfQ=s360",
+        imageUrlsBySize: {
+          90: "https://lh3.googleusercontent.com/JwhZ5F6bm4I67nr2WFSuv_anMW33tuBcWk1n7zzu6zIaAJraUPoogaJexrDUKsIQ2nrEWwtmxlFGSr533anG=s90-c",
+          360: "https://lh3.googleusercontent.com/JwhZ5F6bm4I67nr2WFSuv_anMW33tuBcWk1n7zzu6zIaAJraUPoogaJexrDUKsIQ2nrEWwtmxlFGSr533anG=s360-c"
+        }
+      }
+    ],
+    name: "The Best (and Easy) Peanut Butter Cookies",
+    source: {
+      "sourceDisplayName": "Julia's Album",
+      "sourceSiteUrl": "juliasalbum.com",
+      "sourceRecipeUrl": "https://juliasalbum.com/easy-peanut-butter-cookies/"
+    },
+    id: "The-Best-_and-Easy_-Peanut-Butter-Cookies-2665930",
+    ingredientLines: [
+      "1/4 cup creamy peanut butter (can use natural one too)",
+      "2 tablespoons unsalted butter , room temperature (not melted - just let the butter from refrigerator sit on the counter for about an hour or so)",
+      "1/2 cup brown sugar",
+      "1 egg",
+      "1/2 teaspoon pure vanilla extract",
+      "3/4 cup all-purpose flour , sifted or aerated (see important note below!)",
+      "1/2 teaspoon baking powder",
+      "2 tablespoons granulated sugar for coating"
+    ],
+    attribution: {
+      html: "<a href='http://www.yummly.co/recipe/The-Best-_and-Easy_-Peanut-Butter-Cookies-2665930'>The Best (and Easy) Peanut Butter Cookies recipe</a> information powered by <img alt='Yummly' src='https://static.yummly.co/api-logo.png'/>",
+      url: "http://www.yummly.co/recipe/The-Best-_and-Easy_-Peanut-Butter-Cookies-2665930",
+      text: "The Best (and Easy) Peanut Butter Cookies recipes: information powered by Yummly",
+      logo: "https://static.yummly.co/api-logo.png"
+    },
+    numberOfServings: 2,
+    totalTimeInSeconds: 2400,
+    attributes: {
+      course: [
+        "Afternoon Tea",
+        "Desserts"
+      ],
+      cuisine: [
+        "Kid-Friendly"
+      ]
+    },
+    flavors: {
+      Piquant: 0,
+      Meaty: 0.8333,
+      Bitter: 0.1667,
+      Sweet: 0.8333,
+      Sour: 0,
+      Salty: 0.3333
+    },
+    rating: 4
+  }
+];
+
+export const mockRecipe = {
+  yield: "serves 16-20",
+  totalTime: "30 min",
+  images: [
+    {
+      hostedSmallUrl: "https://lh3.googleusercontent.com/923lxP6iaPgt7L1ZbNQekJGjcoC6CYGMVjQZ_-NEsrw_V5rcKZLcNgyUuLv_jmRV9lkSN0mTbVz95ASXf4LawA=s90",
+      hostedMediumUrl: "https://lh3.googleusercontent.com/923lxP6iaPgt7L1ZbNQekJGjcoC6CYGMVjQZ_-NEsrw_V5rcKZLcNgyUuLv_jmRV9lkSN0mTbVz95ASXf4LawA=s180",
+      hostedLargeUrl: "https://lh3.googleusercontent.com/923lxP6iaPgt7L1ZbNQekJGjcoC6CYGMVjQZ_-NEsrw_V5rcKZLcNgyUuLv_jmRV9lkSN0mTbVz95ASXf4LawA=s360",
+      imageUrlsBySize: {
+        90: "https://lh3.googleusercontent.com/TvJfjV21XRXRSrS5qNcbtGRm7VGSTK2GY-bCAQnlodFjP2ZljyTeuUg7dmvR8VnhubINi4wKBQ10wKRGpPRXgDQ=s90-c",
+        360: "https://lh3.googleusercontent.com/TvJfjV21XRXRSrS5qNcbtGRm7VGSTK2GY-bCAQnlodFjP2ZljyTeuUg7dmvR8VnhubINi4wKBQ10wKRGpPRXgDQ=s360-c"
+      }
+    }
+  ],
+  name: "Frozen Oreo Dessert",
+  source: {
+    sourceDisplayName: "Creations by Kara",
+    sourceSiteUrl: "creationsbykara.com",
+    sourceRecipeUrl: "https://www.creationsbykara.com/frozen-oreo-ice-cream-dessert/"
+  },
+  id: "Frozen-Oreo-Dessert-2621007",
+  ingredientLines: [
+    "1/4 cup butter, melted",
+    "1 package of Oreos, finely crushed (I used my food processor)",
+    "1/2 gallon of ice cream (Cookies & Cream & Chocolate Chip Mint are yummy)",
+    "Hot fudge sauce"
+  ],
+  attribution: {
+    html: "<a href='http://www.yummly.co/recipe/Frozen-Oreo-Dessert-2621007'>Frozen Oreo Dessert recipe</a> information powered by <img alt='Yummly' src='https://static.yummly.co/api-logo.png'/>",
+    url: "http://www.yummly.co/recipe/Frozen-Oreo-Dessert-2621007",
+    "text": "Frozen Oreo Dessert recipes: information powered by Yummly",
+    logo: "https://static.yummly.co/api-logo.png"
+  },
+  numberOfServings: 18,
+  totalTimeInSeconds: 1800,
+  attributes: {
+    course: [
+      "Desserts"
+    ]
+  },
+  flavors: {},
+  rating: 4
+};
+
+export const mockAttribution = { 
+  attribution: {
+  html: "Recipe search powered by <a href='http://www.yummly.co/recipes'><img alt='Yummly' src='https://static.yummly.co/api-logo.png'/></a>",
+  url: "http://www.yummly.co/recipes/",
+  text: "Recipe search powered by Yummly",
+  logo: "https://static.yummly.co/api-logo.png"
+    }
+  }
