96d65febe15300c5ea3019fc51d6423ff8bb80e6
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-23 14:01:58 -0700

    Refactor fetchRecipes to add notes property to fetched data

--- src/thunks/fetchRecipes.js
+++ src/thunks/fetchRecipes.js
@@ -9,8 +9,10 @@ export const fetchRecipes = (recipes) => {
       try {
         const url = `http://api.yummly.com/v1/api/recipe/${recipe.id}?_app_id=${apiId}&_app_key=${apiKey}`;
         const data = await fetchData(url);
+        const notes = recipe.notes || '';
+        const dataWithNotes = await {...data, notes }
         dispatch(toggleLoading(false));
-        return data;
+        return dataWithNotes;
       } catch(error) {
         dispatch(setError(error));
       }
