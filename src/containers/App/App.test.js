561ea64e898fd25a4d2e79861939cc0dc81f6222
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-23 09:00:37 -0700

    Update App test

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { App } from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { rootReducer } from '../../reducers/';
+import { shallow } from 'enzyme';
+import * as data from '../../mockData';
+import { fetchMatches } from '../../thunks/fetchMatches';
+import { fetchUserRecipes } from '../../thunks/fetchUserRecipes';
+
+const mockProps = {
+  desserts: data.mockRecipes,
+  userRecipes: [],
+  error: '',
+  isLoading: false,
+  attribution: {},
+  fetchMatches: jest.fn(() => true),
+  fetchUserRecipes: jest.fn(() => true)
+}
+
+jest.mock('../../thunks/fetchUserRecipes.js');
+jest.mock('../../thunks/fetchMatches.js');
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const store = createStore(rootReducer);
+  ReactDOM.render(<Provider store={store}><BrowserRouter><App {...mockProps}/></BrowserRouter></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
