eaf2db8b448ce0221749451f90e0ea02ee1f61fa
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-22 20:52:04 -0700

    Update userRecipeReducer with cases to add a recipe and update arecipe

--- src/reducers/userRecipesReducer.js
+++ src/reducers/userRecipesReducer.js
@@ -1,5 +1,15 @@
 export const userRecipesReducer = (state = [], action) => {
   switch(action.type) {
+    case 'ADD_RECIPE':
+      return [...state, action.recipe];
+    case 'UPDATE_RECIPE':
+      const updateRecipes = state.map(recipe => {
+        if(recipe.id === action.recipe.id) {
+          return action.recipe
+        }
+        return recipe
+      });
+      return updateRecipes;
     default:
       return state;
   }
