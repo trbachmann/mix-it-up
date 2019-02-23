9953f8cdb32bc06fc06a29ce38d8b400608c37a2
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-23 16:03:22 -0700

    Remove userRecipesReducer from rootReducer all recipes now in desserts

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import { dessertsReducer } from './dessertsReducer';
-import { userRecipesReducer } from './userRecipesReducer';
 import { errorReducer } from './errorReducer';
 import { statusReducer } from './statusReducer';
 import { isLoadingReducer } from './isLoadingReducer';
@@ -8,7 +7,6 @@ import { attributionReducer } from './attributionReducer';
 
 export const rootReducer = combineReducers({
   desserts: dessertsReducer,
-  userRecipes: userRecipesReducer,
   error: errorReducer,
   status: statusReducer,
   isLoading: isLoadingReducer,
