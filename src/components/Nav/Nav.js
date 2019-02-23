4c36dea16b134617db8f0ee0b379198210b1e5aa
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-23 14:09:05 -0700

    Update NavLink to paths

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom';
 export const Nav = () => {
   return(
     <div className='Nav--div'>
-      <NavLink className='Nav--NavLink--margin' to={'/'}>Home</NavLink>
-      <NavLink className='Nav--NavLink' to={'/my-recipes'}>My Recipes</NavLink>
+      <NavLink className='Nav--NavLink--margin' to='/'>Home</NavLink>
+      <NavLink className='Nav--NavLink' to='/my-recipes'>My Recipes</NavLink>
     </div>
   )
 }
