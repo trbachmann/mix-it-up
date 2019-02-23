c3c3077909e64aa76dbca3f44e04d84854a46d7d
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-23 08:51:02 -0700

    Update App to populateUserRecipes if there are any saved in localStorage

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { withRouter, Switch, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { fetchMatches } from '../../thunks/fetchMatches';
+import { fetchUserRecipes } from '../../thunks/fetchUserRecipes';
 import RecipeContainer from '../RecipeContainer/RecipeContainer.js';
 import RecipeCard from '../RecipeCard/RecipeCard.js';
 import { Error404 } from '../../components/Error404/Error404.js';
@@ -13,6 +14,15 @@ export class App extends Component {
     if (!this.props.desserts.length) {
     this.props.fetchMatches();
     }
+
+    if (localStorage.hasOwnProperty('userRecipes')) {
+      this.populateUserRecipes();
+    }
+  }
+
+  populateUserRecipes = () => {
+    const savedUserRecipes = JSON.parse(localStorage.getItem('userRecipes'));
+    this.props.fetchUserRecipes(savedUserRecipes);
   }
   
   render() {
@@ -25,15 +35,6 @@ export class App extends Component {
         <Switch>
           <Route exact path='/desserts' render={({ match }) => <RecipeContainer match={match}/>} />
           <Route exact path='/my-recipes' render={({ match }) => <RecipeContainer match={match}/>} />
-          <Route exact path='/' render={({ match }) => <RecipeContainer match={match}/>}/>
-          <Route path='/desserts/:id' render={({ match }) => {
-            const dessert= this.props.desserts.find(dessert => dessert.id === match.params.id);
-            if (!dessert) {
-              return <Error404 />
-            }
-            return <RecipeCard match={match} recipe={dessert}/>
-          }} />
-        </Switch>
           <Route path='/my-recipes/:id' render={({ match }) => {
             console.log(match.params.id);
             const userRecipe = this.props.userRecipes.find(recipe => recipe.id === match.params.id);
@@ -42,6 +43,15 @@ export class App extends Component {
             }
             return <RecipeCard match={match} recipe={userRecipe} />
           }} />
+          <Route path='/desserts/:id' render={({ match }) => {
+            const dessert= this.props.desserts.find(dessert => dessert.id === match.params.id);
+            if (!dessert) {
+              return <Error404 />
+            }
+            return <RecipeCard match={match} recipe={dessert}/>
+          }} />
+          <Route exact path='/' render={({ match }) => <RecipeContainer match={match}/>}/>
+        </Switch>
       </div>
     );
   }
@@ -56,7 +66,18 @@ export const mapStateToProps = (state) => ({
 });
 
 export const mapDispatchToProps = (dispatch) => ({
-  fetchMatches: () => dispatch(fetchMatches())
+  fetchMatches: () => dispatch(fetchMatches()),
+  fetchUserRecipes: (recipes) => dispatch(fetchUserRecipes(recipes))
 });
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
+
+App.propTypes = {
+  desserts: PropTypes.array,
+  userRecipes: PropTypes.array,
+  error: PropTypes.string,
+  isLoading: PropTypes.bool,
+  attribution: PropTypes.object,
+  fetchMatches: PropTypes.func,
+  fetchUserRecipes: PropTypes.func
+}
