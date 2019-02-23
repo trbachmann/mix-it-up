bfb8cd9ee20cdc73b2a2a46ecb6ef03246a7b13e
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-23 14:07:06 -0700

    Refactor RecipeContainer to break out getRecipesToDisplay

--- src/containers/RecipeContainer/RecipeContainer.js
+++ src/containers/RecipeContainer/RecipeContainer.js
@@ -3,25 +3,35 @@ import { connect } from 'react-redux';
 import { RecipeLinkCard } from '../../components/RecipeLinkCard/RecipeLinkCard';
 
 export class RecipeContainer extends Component {
-  getRecipeType = () => {
+  getRecipesToDisplay = () => {
     const { match } = this.props;
     switch(match.path) {
       case '/my-recipes':
-        return 'userRecipes';
+        return this.populateUserRecipes();
       case '/desserts':
-        return 'desserts';
       case '/':
-        return 'desserts';
+        return this.populateRecipesWithoutUsers();
       default:
-        return 'desserts';
+        return this.populateRecipesWithoutUsers();
     }
   }
   
-  getRecipesToDisplay = () => {
-    let property = this.getRecipeType();  
-    return this.props[property].map(recipe => {
-      return <RecipeLinkCard recipe={recipe} key={recipe.id} type={property}/>
-    });
+  populateUserRecipes = () => {
+    return this.props.desserts.reduce((acc, recipe) => {
+      if (recipe.notes !== '') {
+        acc.push(<RecipeLinkCard recipe={recipe} key={recipe.id} />)
+      }
+      return acc;
+    }, []);
+  }
+
+  populateRecipesWithoutUsers = () => {
+    return this.props.desserts.reduce((acc, recipe) => {
+      if (recipe.notes === '') {
+        acc.push(<RecipeLinkCard recipe={recipe} key={recipe.id} />)
+      }
+      return acc;
+    }, []);
   }
 
   render() {
@@ -29,13 +39,12 @@ export class RecipeContainer extends Component {
       <div className='RecipeContainer'>
       {this.getRecipesToDisplay()}
       </div>
-    )
+    );
   }
 }
 
 export const mapStateToProps = (state) => ({
-  desserts: state.desserts,
-  userRecipes: state.userRecipes
+  desserts: state.desserts
 });
 
 export default connect(mapStateToProps)(RecipeContainer);
