95a54c32965b735323a4aabffd29ff07b88836dd
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-23 16:02:40 -0700

    Remove userRecipesReducer and update dessertsReducer

--- src/reducers/dessertsReducer.js
+++ src/reducers/dessertsReducer.js
@@ -2,6 +2,21 @@ export const dessertsReducer = (state = [], action) => {
   switch(action.type) {
     case 'SET_RECIPES':
       return action.recipes;
+    case 'UPDATE_RECIPE_NOTES':
+      const updatedRecipes = state.map(recipe => {
+        if (recipe.id === action.id) {
+          recipe.notes = action.notes;
+        }
+        return recipe;
+      });
+      return updatedRecipes;
+    case 'DELETE_RECIPE_NOTES':
+      const recipesWithNoteRemoved = state.map(recipe => {
+        if (recipe.id === action.id) {
+          recipe.note = '';
+        }
+      });
+      return recipesWithNoteRemoved;
     default:
       return state;
   }
