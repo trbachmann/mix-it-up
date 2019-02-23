c4f8dacc33ef146f57ab455063764a1b5a6ac621
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-23 14:06:16 -0700

    Refactor fetchMatches to fetchRecipesAndAttribution

--- src/thunks/fetchRecipesAndAttribution.js
+++ src/thunks/fetchRecipesAndAttribution.js
@@ -2,20 +2,21 @@ import { fetchData } from '../utils/fetchData';
 import { toggleLoading, setRecipes, setAttribution, setError } from '../actions';
 import { apiId, apiKey } from '../api-key';
 import { fetchRecipes } from './fetchRecipes';
+import { combineMatchesAndUserRecipes } from '../utils/combineMatchesAndUserRecipes';
 
-export const fetchMatches= () => {
+export const fetchRecipesAndAttribution = (userRecipes) => {
   return async (dispatch) => {
     try {
       dispatch(toggleLoading(true));
-      const url = `http://api.yummly.com/v1/api/recipes?_app_id=${apiId}&_app_key=${apiKey}&q=desserts&requirePictures=true&maxResult=40&start=40`;
+      const url = `http://api.yummly.com/v1/api/recipes?_app_id=${apiId}&_app_key=${apiKey}&q=desserts&requirePictures=true&maxResult=4&start=40`;
       const response = await fetchData(url);
+      const recipesToFetch = await combineMatchesAndUserRecipes(response.matches, userRecipes);
+      const recipes = await dispatch(fetchRecipes(recipesToFetch));
       dispatch(toggleLoading(false));
-      const recipes = await dispatch(fetchRecipes(response.matches));
       dispatch(setRecipes(recipes));
       dispatch(setAttribution(response.attribution));
     } catch(error) {
       dispatch(setError(error));
     }
-    
   }
 }
