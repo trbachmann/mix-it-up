e8083e933aa96910bd37f0f7c7b013e3df801567
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-23 14:08:50 -0700

    Refactor RecipeLinkCard to change pathToUse based on recipe.notes

--- src/components/RecipeLinkCard/RecipeLinkCard.js
+++ src/components/RecipeLinkCard/RecipeLinkCard.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const RecipeLinkCard = (props) => {
-  const { recipe, type } = props;
+  const { recipe } = props;
   let pathToUse;
-  (type === 'desserts') ? (pathToUse = '/desserts/') : (pathToUse = '/my-recipes/')
+  (recipe.notes === '') ? (pathToUse = '/desserts/') : (pathToUse = '/my-recipes/')
   return(
-    <Link className='RecipeLinkCard--Link' to={pathToUse+ recipe.id}>
+    <Link className='RecipeLinkCard--Link' to={pathToUse + recipe.id}>
       <div className='RecipeLinkCard--div'>
         <img className='RecipeLinkCard-img' src={recipe.images[0].hostedLargeUrl} alt={recipe.name}/>
         <h2>{recipe.name}</h2>
