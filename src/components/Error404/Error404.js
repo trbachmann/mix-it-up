66133ccb2af9de62b747a907ca5de71d97e3d805
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-24 10:44:52 -0700

    Style Error404 and add snapshot test. Resolves #4

--- src/components/Error404/Error404.js
+++ src/components/Error404/Error404.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import blankRecipe from '../../images/kara-eads-unsplash.jpg'
 export const Error404 = () => {
   return(
     <div className='Error404'>
       <h1>No recipe found</h1>
+      <img className='Error404--img' src={blankRecipe} alt='Empty recipe book and measuring cups'/>
+      <p className='Error404--p--credit'>Photo by Kara Eads on Unsplash</p>
     </div>
   );
 }
