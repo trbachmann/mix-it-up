305889d87f77ad3bb0d0bb4ab8342f341386b280
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-24 18:41:37 -0700

    Add App testst for mapStateToProps and mapDispatchToProps

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './App';
+import { App, mapStateToProps, mapDispatchToProps } from './App';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { createStore } from 'redux';
@@ -20,9 +20,43 @@ const mockProps = {
 
 jest.mock('../../thunks/fetchRecipesAndAttribution.js');
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  const store = createStore(rootReducer);
-  ReactDOM.render(<Provider store={store}><BrowserRouter><App {...mockProps}/></BrowserRouter></Provider>, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  describe('App component', () => {
+    it('renders without crashing', () => {
+      const div = document.createElement('div');
+      const store = createStore(rootReducer);
+      ReactDOM.render(<Provider store={store}><BrowserRouter><App {...mockProps}/></BrowserRouter></Provider>, div);
+      ReactDOM.unmountComponentAtNode(div);
+    });
+  });
+
+  describe('mapStateToProps', () => {
+    it('should return an object with desserts, error, isLoadind and attribution', () => {
+      const mockState = {
+        desserts: data.mockRecipes,
+        error: '',
+        status: '',
+        isLoading: false,
+        attribution: data.mockAttribution,
+      };
+      const expected = {
+        desserts: data.mockRecipes,
+        error: '',
+        isLoading: false,
+        attribution: data.mockAttribution,
+      };
+      const mappedProps = mapStateToProps(mockState);
+      expect(mappedProps).toEqual(expected);
+    });
+  });
+
+  describe('mapDispatchToProps', () => {
+    it('should call dispatch with a fetchRecipesAndAttribution', () => {
+      const mockDispatch = jest.fn();
+      const actionToDispatch = fetchRecipesAndAttribution([ data.mockUserRecipeFromStorage ]);
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.fetchRecipesAndAttribution([ data.mockUserRecipeFromStorage ]);
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
+  });
 });
