6eafbecd120b300562c9dc00d42f4a4034192fb0
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-25 12:07:27 -0700

    Update mockData and fetchRecipes

--- src/thunks/fetchRecipes.js
+++ src/thunks/fetchRecipes.js
@@ -10,7 +10,7 @@ export const fetchRecipes = (recipes) => {
         const url = `http://api.yummly.com/v1/api/recipe/${recipe.id}?_app_id=${apiId}&_app_key=${apiKey}`;
         const data = await fetchData(url);
         const notes = recipe.notes || '';
-        const dataWithNotes = await {...data, notes }
+        const dataWithNotes = {...data, notes }
         dispatch(toggleLoading(false));
         return dataWithNotes;
       } catch(error) {
