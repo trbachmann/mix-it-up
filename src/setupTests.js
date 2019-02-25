ac6a87fed742fcf0ea2ffa4e43870c565ab25944
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-25 13:20:00 -0700

    Add mock localSotage for testing

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,3 +2,23 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+class LocalStorage {
+  constructor() {
+    this.store = { }
+  }
+
+  getItem(key) {
+    return this.store[key];
+  }
+
+  setItem(key, string) {
+    this.store[key] = string;
+  }
+
+  hasOwnProperty(string) {
+    return this.store[string] ? true : false;
+  }
+}
+
+global.localStorage = new LocalStorage;
