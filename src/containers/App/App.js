1bb483e084cb9158895c10fa9388728258e5c381
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-24 18:38:17 -0700

    Adds Loading component to App and update imports for main.scss

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -7,6 +7,7 @@ import RecipeContainer from '../RecipeContainer/RecipeContainer.js';
 import RecipeCard from '../RecipeCard/RecipeCard.js';
 import { Error404 } from '../../components/Error404/Error404.js';
 import { Nav } from '../../components/Nav/Nav.js';
+import { Loading } from '../../components/Loading/Loading';
 
 export class App extends Component {
   calculateTotalUserRecipes = () => {
@@ -16,6 +17,7 @@ export class App extends Component {
     }, 0);
     return totalUserRecipes;
   }
+  
   componentDidMount() {
     const { fetchRecipesAndAttribution } = this.props;
     if (localStorage.hasOwnProperty('userRecipes')) {
@@ -31,24 +33,26 @@ export class App extends Component {
     const dessert = desserts.find(dessert => dessert.id === match.params.id);
     if (!dessert) {
       return <Error404 />
-    }
+    } 
     return <RecipeCard match={match} recipe={dessert} />
   }
   
   render() {
-
+    const { isLoading } = this.props;
     return (
       <div className="App">
         <header className="App--header">
           <h1>Mix It Up</h1>
           <Nav totalUserRecipes={this.calculateTotalUserRecipes()}/>
         </header>
+        { isLoading && <Loading />}
         <Switch>
           <Route exact path='/desserts' render={({ match }) => <RecipeContainer match={match}/>} />
           <Route exact path='/my-recipes' render={({ match }) => <RecipeContainer match={match}/>} />
           <Route path='/my-recipes/:id' render={this.getRecipeRoute} />
           <Route path='/desserts/:id' render={this.getRecipeRoute} />
           <Route exact path='/' render={({ match }) => <RecipeContainer match={match}/>}/>
+          <Route component={Error404} />
         </Switch>
       </div>
     );
