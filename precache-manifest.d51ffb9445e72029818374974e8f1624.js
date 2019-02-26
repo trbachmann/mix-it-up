f6d472d5388ddc84c1bc36a4618f4e283eed1d61
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-26 16:38:50 -0700

    Updates

---
+++ precache-manifest.d51ffb9445e72029818374974e8f1624.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e8d1d5098958764c85be17901bc4254",
+    "url": "/mix-it-up/static/media/kara-eads-unsplash.9e8d1d50.jpg"
+  },
+  {
+    "revision": "e75f2de6da4e878353b433ade8163034",
+    "url": "/mix-it-up/static/media/charisse-kenion-unsplash.e75f2de6.jpg"
+  },
+  {
+    "revision": "97871cdb096fc661e4bb",
+    "url": "/mix-it-up/static/js/runtime~main.97871cdb.js"
+  },
+  {
+    "revision": "1ac476084478714bf5fe",
+    "url": "/mix-it-up/static/js/main.1ac47608.chunk.js"
+  },
+  {
+    "revision": "6783c334a9a884ce9bb5",
+    "url": "/mix-it-up/static/js/2.6783c334.chunk.js"
+  },
+  {
+    "revision": "1ac476084478714bf5fe",
+    "url": "/mix-it-up/static/css/main.cd109af3.chunk.css"
+  },
+  {
+    "revision": "ee2d653d88fa8d74c3f0e2b8ca4854d1",
+    "url": "/mix-it-up/index.html"
+  }
+];
