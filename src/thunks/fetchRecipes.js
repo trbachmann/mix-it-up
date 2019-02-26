d7c1105e85e67bd8f2c8c114e3526f2c3b9c1a13
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-26 16:50:32 -0700

    Fix http to https

--- src/thunks/fetchRecipes.js
+++ src/thunks/fetchRecipes.js
@@ -7,7 +7,7 @@ export const fetchRecipes = (recipes) => {
     dispatch(toggleLoading(true));
     const unresolvedRecipes = recipes.map(async (recipe) => {
       try {
-        const url = `http://api.yummly.com/v1/api/recipe/${recipe.id}?_app_id=${apiId}&_app_key=${apiKey}`;
+        const url = `https://api.yummly.com/v1/api/recipe/${recipe.id}?_app_id=${apiId}&_app_key=${apiKey}`;
         const data = await fetchData(url);
         const notes = recipe.notes || '';
         const dataWithNotes = {...data, notes }
