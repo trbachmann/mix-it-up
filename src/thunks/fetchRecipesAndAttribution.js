c4a44a9e1b955ee6072474acb29b8cc288b87971
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-04-11 12:27:21 -0600

    Refactor fetchRecipesAndAttribution

--- src/thunks/fetchRecipesAndAttribution.js
+++ src/thunks/fetchRecipesAndAttribution.js
@@ -7,12 +7,17 @@ export const fetchRecipesAndAttribution = (userRecipes) => {
   return async (dispatch) => {
     try {
       dispatch(toggleLoading(true));
-      const url = 'https://api.yummly.com/v1/api/recipes?_app_id=' + process.env.REACT_APP_API_ID + '&_app_key=' + process.env.REACT_APP_API_KEY + '&q=desserts&requirePictures=true&maxResult=30&start=40';
+      const url = 'https://api.yummly.com/v1/api/recipes?_app_id=' + process.env.REACT_APP_API_ID + '&_app_key=' + process.env.REACT_APP_API_KEY + '&q=desserts&requirePictures=true&maxResult=32&start=40';
       const response = await fetchData(url);
       const recipesToFetch = await combineMatchesAndUserRecipes(response.matches, userRecipes);
       const recipes = await dispatch(fetchRecipes(recipesToFetch));
+      const updateRecipes = recipes.filter(
+        recipe =>
+          recipe.id !==
+          'No-Bake-Keto-Peanut-Butter-Chocolate-Bars-2537418'
+      );
       dispatch(toggleLoading(false));
-      dispatch(setRecipes(recipes));
+      dispatch(setRecipes(updateRecipes));
       dispatch(setAttribution(response.attribution));
     } catch(error) {
       dispatch(setError(error.message));
